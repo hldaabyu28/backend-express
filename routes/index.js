d356da96390587fe6860a2ad4351af2ceec38218
Hilda Abiyyu Rahmadani <danirahmadani91@gmail.com>
2025-05-08 20:48:22 +0700

    add auth

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,19 @@ const express = require("express");
 const router = express.Router();
 
 // import register controller
-const RegisterController = require("../controllers/RegisterController");
+const registerController = require("../controllers/RegisterController");
+
+// import login controller
+const loginController = require("../controllers/LoginController");
 
 // import validator register
-const { validateRegister } = require("../utils/validators/auth");
+const { validateRegister, validateLogin } = require("../utils/validators/auth");
 
 // define router for register
-router.post('/register', validateRegister, RegisterController.register);
+router.post('/register', validateRegister, registerController.register);
+
+// define router for login
+router.post('/login', validateLogin,loginController.login);
 
 // export router
 module.exports = router
